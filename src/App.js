258e9cfa658bc75458328c5945beff98a7ab1cc4
Anudeep <anudeep.hanuman@gmail.com>
2023-06-19 16:23:27 +0530

    qts website done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import {Container, Card, Row, Col, Button} from "react-bootstrap";
+import {Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+import cse from "./cse.jpg"
+import ece from "./ece.jpeg"
+import eee from "./eee.jpg"
+import mech from "./mech.jpg"
+import chem from "./chem.jpg"
+import civil from "./civil.jpg"
+import logo from "./logo.jpg"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="logo" src={logo} alt="logo" />
+        <h1> Quest Test Series 2023 </h1>
       </header>
+
+      <Container>
+        <Row>
+          <Col xs={12} sm={6} md={4}>
+            <Card className="card">
+              <Card.Img variant="top" src={cse} />
+              <Card.Body>
+                <Card.Title>CSE/ISE/AIDS/AIML</Card.Title>
+                <Card.Text></Card.Text>
+                <Link to="https://unstop.com/p/quest-test-series-csisadai-quest-690260" target="__blank" className="btn btn-success">Go &rarr;</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} sm={6} md={4}>
+            <Card>
+              <Card.Img variant="top" src={ece} />
+              <Card.Body>
+                <Card.Title>EC/EI/ET</Card.Title>
+                <Card.Text></Card.Text>
+                <Link to="https://unstop.com/p/quest-test-series-eceiet-quest-690292" target="__blank" className="btn btn-success">Go &rarr;</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} sm={6} md={4}>
+            <Card>
+              <Card.Img variant="top" src={eee} />
+              <Card.Body>
+                <Card.Title>EEE</Card.Title>
+                <Card.Text></Card.Text>
+                <Link to="https://unstop.com/p/quest-test-series-eee-quest-690296" target="__blank" className="btn btn-success">Go &rarr;</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} sm={6} md={4}>
+            <Card>
+              <Card.Img variant="top" src={mech} />
+              <Card.Body>
+                <Card.Title>Mechanical/IEM</Card.Title>
+                <Card.Text></Card.Text>
+                <Link to="https://unstop.com/p/quest-test-series-2023-mechanical-quest-690172" target="__blank" className="btn btn-success">Go &rarr;</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} sm={6} md={4}>
+            <Card>
+              <Card.Img variant="top" src={chem} />
+              <Card.Body>
+                <Card.Title>Chemical/Biotechnology</Card.Title>
+                <Card.Text></Card.Text>
+                <Link to="https://unstop.com/p/quest-test-series-chembiotech-quest-690257" target="__blank" className="btn btn-success">Go &rarr;</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} sm={6} md={4}>
+            <Card>
+              <Card.Img variant="top" src={civil} />
+              <Card.Body>
+                <Card.Title>Civil</Card.Title>
+                <Card.Text></Card.Text>
+                <Link to="https://unstop.com/p/quest-test-series-2023-civil-quest-690268" target="__blank" className="btn btn-success">Go &rarr;</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      <footer className="footer">
+        <h5>&#169; Team Quest 2023</h5>
+      </footer>
     </div>
   );
 }
